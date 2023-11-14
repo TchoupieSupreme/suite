a8d186cd5eec59c90919d0ebc2f4cfcf32d5880d
TchoupieSupreme <passion.johan@gmail.com>
2023-11-14 09:11:07 +0100

    suite2; suite3

--- src/main/java/ovh/johanpassion/App.java
+++ src/main/java/ovh/johanpassion/App.java
@@ -12,23 +12,36 @@ public class App {
             int nb =0;
         for(int i =-4; nb<86;i+=18){
             nb=i;
-            System.out.println(nb);
+            System.out.print(nb+" ");
         }
         }
 
         public static void suite2() {
             int nombreUn = 0;
             int nombreDeux = 1;
-            while (nombreUn!=20) {
-                
+            int i=0;
+            while (i!=20) {
+                System.out.print(" "+nombreUn+" "+nombreDeux);
+                nombreUn+=nombreDeux;
+                nombreDeux+=nombreUn;
+                i++;
             }
             
+        }
+
+        public static void suite3(){
+            for(int i=0; i<30;i++){
+                System.out.print((Math.pow(i, 2))+" ");
+                
+            }
         }
     
         
     }
     public static void main(String[] args) {
-        main.suite1();
+        //main.suite1();
+        //main.suite2();
+        main.suite3();
         
     }
     
