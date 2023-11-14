a0f26e55ee412dfb24c5ae8ea8bd21fe45d96953
TchoupieSupreme <passion.johan@gmail.com>
2023-11-14 08:53:54 +0100

    first commit

---
+++ src/main/java/ovh/johanpassion/App.java
@@ -0,0 +1,36 @@
+package ovh.johanpassion;
+
+public class App {
+    /**
+     * main
+ 
+ public static void suite1(){
+    
+ }    */
+    public static class main {
+        public static void suite1(){
+            int nb =0;
+        for(int i =-4; nb<86;i+=18){
+            nb=i;
+            System.out.println(nb);
+        }
+        }
+
+        public static void suite2() {
+            int nombreUn = 0;
+            int nombreDeux = 1;
+            while (nombreUn!=20) {
+                
+            }
+            
+        }
+    
+        
+    }
+    public static void main(String[] args) {
+        main.suite1();
+        
+    }
+    
+    
+}
